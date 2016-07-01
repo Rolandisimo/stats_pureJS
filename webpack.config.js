18c0a1a7bae50b832d856ab36084266007d8799d
Roland Jegorov <rolandjegorov@Rolands-MacBook-Pro.local>
2016-07-01 19:04:48 +0300

    Changed Babel 5 to Babel 6. Edited webpack.config accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,34 @@ var path = require('path');
 var projectRoot = __dirname;
 
 var config = {
-  // Must be an absolute path or watching feature will fail
-  context: path.join(projectRoot, 'app'),
-  entry: './index.js',
-  output: {
-    path: path.join(projectRoot, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-        { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-        { test: /\.css$/, loader: 'style!css'},
-    ],
-  },
-  resolveLoader: {
-    root: [
-      path.join(projectRoot, 'node_modules'),
-    ],
-  },
-  resolve: {
-    root: [
-      path.join(projectRoot, 'node_modules'),
-    ],
-  },
+    // Must be an absolute path or watching feature will fail
+    context: path.join(projectRoot, 'app'),
+    entry: ['babel-polyfill', './index.js'],
+    output: {
+        path: path.join(projectRoot, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015', 'stage-0']
+                }
+            }, {
+                test: /\.css$/,
+                loader: 'style!css'
+            }
+        ]
+    },
+    resolveLoader: {
+        root: [path.join(projectRoot, 'node_modules')]
+    },
+    resolve: {
+        root: [path.join(projectRoot, 'node_modules')]
+    }
 };
 module.exports = config;
